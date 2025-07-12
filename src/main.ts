4ccd120bd8a6e12d2e60d2dca096214582b01580
UstymUkhman <ustym.ukhman@gmail.com>
2025-07-12 14:35:32 +0300

    Update Resize Event Handler

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,25 @@
 import Worker from "./Worker?worker";
 
+function resize()
+{
+    // Mimic CSS canvas size settings:
+    const maxCanvasSize = +getComputedStyle(document.documentElement)
+        .getPropertyValue("--maxCanvasWidth").slice(0, -2);
+
+    const width = Math.min(maxCanvasSize, innerWidth);
+    const height = Math.min(width / 4 * 3, innerHeight);
+
+    worker.postMessage({ action: "Resize::Window", width: height / 3 * 4, height });
+}
+
 const worker = new Worker();
+worker.onerror = console.error;
+worker.onmessage = () => resize();
+
+addEventListener("resize", resize, false);
 
 const canvas2D = output.transferControlToOffscreen();
 const canvasWebGPU = scene.transferControlToOffscreen();
 
-worker.postMessage({
-    action: "Transfer::WebGPU",
-    canvas: canvasWebGPU,
-    width: innerWidth,
-    height: innerHeight
-}, [canvasWebGPU]);
-
 worker.postMessage({ action: "Transfer::2D", canvas: canvas2D }, [canvas2D]);
-
-addEventListener("resize", () =>
-{
-    let width = Math.min(innerWidth, 1024);
-    let height = width / 4 * 3;
-
-    if (innerHeight < height)
-    {
-        height = innerHeight;
-        width = height / 3 * 4;
-    }
-
-    canvas2D.width = width;
-    canvas2D.height = height;
-
-    canvasWebGPU.width = width;
-    canvasWebGPU.height = height;
-
-    worker.postMessage({ action: "Resize::Window", width, height });
-}, false);
+worker.postMessage({ action: "Transfer::WebGPU", canvas: canvasWebGPU }, [canvasWebGPU]);
--- src/worker.ts
+++ src/worker.ts
@@ -2,14 +2,14 @@ import Scene from "./Scene";
 
 const scene = new Scene();
 
-self.onmessage = ({ data }) =>
+self.onmessage = async ({ data }) =>
 {
     switch (data.action)
     {
         case "Transfer::WebGPU":
-            return scene.create(data.canvas).then(() => setTimeout(() =>
-                scene.resize(data.width, data.height)
-            , 50 / 3));
+            await scene.create(data.canvas);
+            self.postMessage({ action: "Resize::Window" });
+        break;
 
         case "Transfer::2D":
             return scene.output = data.canvas;
