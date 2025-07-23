e8717572312750b8b974120a12a3b15ef9a9248d
UstymUkhman <ustym.ukhman@gmail.com>
2025-07-23 16:36:36 +0300

    Update Project Dependencies

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,18 +1,22 @@
 /// <reference types="vite/client" />
 
+declare type Sphere = {
+    rad: number;
+    p: number[];
+    e: number[];
+    c: number[];
+    refl: number;
+};
+
 declare const scene: HTMLCanvasElement;
 declare const output: HTMLCanvasElement;
 
-declare type StorageBuffer =
-{
-    [name: string]: /* TypedArray */ unknown,
-    buffer: /* GPUBuffer */ unknown,
-};
-
 declare type UniformBuffer<Name extends string,
     TArrayBuffer extends ArrayBufferLike = Float32Array
-> = { buffer: /* GPUBuffer */ unknown } & { [N in Name]: TArrayBuffer };
+> = { buffer: GPUBuffer } & { [N in Name]: TArrayBuffer };
 
 declare type StructBuffer<Name extends string,
     TArrayBuffer extends ArrayBufferLike = Float32Array
-> = { buffer: /* GPUBuffer */ unknown } & { [N in Name]: Record<string, TArrayBuffer> };
+> = { buffer: GPUBuffer } & { [N in Name]: Record<string, TArrayBuffer> };
+
+declare type StorageBuffer = { [name: string]: ArrayBufferLike, buffer: GPUBuffer };
